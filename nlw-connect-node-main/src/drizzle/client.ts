fb5cff1f124377bda6f5f5fef90d0ceeaba4c410
Jonatan Hermann <jonatanhermann@hotmail.com>
2025-08-21 17:52:39 -0300

    configuração do client drizzle

--- nlw-connect-node-main/src/drizzle/client.ts
+++ nlw-connect-node-main/src/drizzle/client.ts
@@ -4,7 +4,9 @@ import { env } from '../env'
 import { schema } from './schema'
 
 export const pg = postgres(env.DATABASE_URL, {
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export const db = drizzle(pg, { schema })
--- nlw-connect-node-main/src/redis/client.ts
+++ nlw-connect-node-main/src/redis/client.ts
@@ -1,9 +1,4 @@
 import { Redis } from 'ioredis'
-import postgres from 'postgres'
 import { env } from '../env'
 
 export const redis = new Redis(env.REDIS_URL)
-
-export const pg = postgres(env.DATABASE_URL, {
-  ssl: true, // Adicione esta linha
-})
