621cb8a75accda4ad377737b536a555af7da9c67
Jonatan Hermann <jonatanhermann@hotmail.com>
2025-08-21 17:42:57 -0300

    configuração do client adicionando ssl true e modificação do env para a criação manual da tabela

--- nlw-connect-node-main/src/redis/client.ts
+++ nlw-connect-node-main/src/redis/client.ts
@@ -1,4 +1,9 @@
 import { Redis } from 'ioredis'
+import postgres from 'postgres'
 import { env } from '../env'
 
 export const redis = new Redis(env.REDIS_URL)
+
+export const pg = postgres(env.DATABASE_URL, {
+  ssl: true, // Adicione esta linha
+})
